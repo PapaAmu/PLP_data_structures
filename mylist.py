91724cca4179796010f9a3b4e1685a7c2c003c3a
Themba Real Lukhele <realthemba@gmail.com>
2024-03-08 10:27:34 +0200

    list lesson complete

--- mylist.py
+++ mylist.py
@@ -1,3 +1,20 @@
+#EMPTY LIST
 my_list = []
 
+#Append list
 my_list.extend([10, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+print(my_list)
+
+
+index_of_30 = my_list.index(30)
+print("index of 30 in my_list is : ", index_of_30)
+
